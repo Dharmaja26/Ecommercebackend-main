540d45603f815901c0a7815894e3db4dd726dad8
Dharmaja26 <165750322+Dharmaja26@users.noreply.github.com>
2024-05-06 13:29:37 +0530

    included nodemon

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const cors = require('cors');
 
+
+// Controllers 
 const userRoutes = require('./routes/userRoutes/index.js');
 
 
